32f4c72d2d4f3c1b18c67d535d1e92aebefe000e
Peter Boerboom <plboerboom@gmail.com>
2015-08-27 08:00:30 -0400

    Fiddling with schemas and record structure.

---
+++ testschmema.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import argparse
+import json
+
+import jsonschema
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('schema')
+    parser.add_argument('instance')
+    args = parser.parse_args()
+
+    with open(args.schema) as f:
+        try:
+            schema = json.load(f)
+        except:
+            raise
+
+    with open(args.instance) as f:
+        try:
+            instance = json.load(f)
+        except:
+            raise
+
+    jsonschema.validate(instance, schema)
